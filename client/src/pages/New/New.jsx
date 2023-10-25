067db75a51db046ccca7300ad7ec9f19f5bef52b
joegilberto <joekgilberto@gmail.com>
2023-10-25 15:30:07 -0400

    stubs out form submission

--- client/src/pages/New/New.jsx
+++ client/src/pages/New/New.jsx
@@ -1,27 +1,72 @@
 import './New.css';
 
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router";
+
+const date = new Date();
+
+const initState = {
+  title: "",
+  steelbook: false,
+  four_k: false,
+  format: "Film",
+  notes: ""
+}
 
 export default function New({ setPage }) {
 
-    useEffect(()=>{
-        setPage("new")
-    },[])
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState(initState);
+
+  function handleChange(e) {
+    let updatedData;
+
+    if (e.target.name === "steelbook" || e.target.name === "four_k"){
+      let value;
+
+      if (e.target.value === "on"){
+        value = true;
+      } else {
+        value = false;
+      }
+      updatedData = { ...formData, [e.target.name]: value }
+    } else {
+      updatedData = { ...formData, [e.target.name]: e.target.value }
+    }
+
+    setFormData(updatedData)
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    console.log(formData)
+    navigate("/index")
+  }
+
+  useEffect(() => {
+    setPage("new")
+  }, [])
 
   return (
     <div className='New'>
-      <form>
-        <label>Title
-          <input type='text' maxlength="50"/>
+      <form onSubmit={handleSubmit}>
+        <label>Title*
+          <input type='text' maxLength="50" name="title" onChange={handleChange} required />
         </label>
         <label className='check'>Special Edition
-          <input className='checkbox' type='checkbox'/>
+          <div className='container'>
+            <input className='checkbox' type='checkbox' name="steelbook" onChange={handleChange} />
+            <span className="checkmark"></span>
+          </div>
         </label>
         <label className='check'>4K
-          <input className='checkbox' type='checkbox'/>
+          <div className='container'>
+            <input className='checkbox' type='checkbox' name="four_k" onChange={handleChange} />
+            <span className="checkmark"></span>
+          </div>
         </label>
         <label>Format
-          <select>
+          <select name="format" onChange={handleChange}>
             <option>Film</option>
             <option>Short</option>
             <option>Television</option>
@@ -29,10 +74,10 @@ export default function New({ setPage }) {
           </select>
         </label>
         <label>Notes
-          <textarea maxlength="255"/>
+          <textarea maxLength="255" name="notes" onChange={handleChange} />
         </label>
-        <button type='submit'>Save</button>
-        </form>
+        <button type='submit'>Collect</button>
+      </form>
     </div>
   );
 }
