1f531e83161d1ac224c2c029b4c35e950203cd60
joegilberto <joekgilberto@gmail.com>
2023-10-25 11:01:10 -0400

    restructures nav placement to be in line with background

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -2,14 +2,13 @@ import './App.css';
 
 import Background from '../components/Background/Background';
 import Main from '../components/Main/Main';
-import Nav from '../components/Menu/Nav';
+import Nav from '../components/Nav/Nav';
 
 export default function App() {
   return (
     <div className="App">
       <Background />
       <Main />
-      <Nav />
     </div>
   );
 }
--- client/src/components/Background/Background.jsx
+++ client/src/components/Background/Background.jsx
@@ -1,9 +1,11 @@
+import Nav from '../Nav/Nav';
 import './Background.css';
 
 export default function Background() {
   return (
     <div className="Background">
         <img src="https://i.imgur.com/sw3dkDJ.png" alt="disc" />
+        <Nav />
     </div>
   );
 }
