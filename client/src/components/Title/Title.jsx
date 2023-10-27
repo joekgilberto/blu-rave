cb0ca50e00614886f9a2be19d9afcdf63f77ddea
joegilberto <joekgilberto@gmail.com>
2023-10-27 12:19:58 -0400

    adds link to logo

--- client/src/components/Title/Title.jsx
+++ client/src/components/Title/Title.jsx
@@ -3,7 +3,9 @@ import './Title.css';
 export default function Title() {
   return (
     <div className="Title">
-      <h1>BLU-RAVE</h1>
+      <a href="/">
+        <h1>BLU-RAVE</h1>
+      </a>
     </div>
   );
 }
