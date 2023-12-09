2df0643678d42ed38b2defb36163d87b760725bf
joekgilberto <joekgilberto@gmail.com>
2023-12-09 14:35:30 -0500

    adds footer

--- client/src/components/Background/Background.jsx
+++ client/src/components/Background/Background.jsx
@@ -1,11 +1,15 @@
 import Nav from '../Nav/Nav';
 import './Background.css';
+import Footer from '../Footer/Footer';
 
-export default function Background({page}) {
+export default function Background({ page }) {
   return (
     <div className="Background">
+      <div className='back-nav'>
         <img src="https://i.imgur.com/sw3dkDJ.png" alt="disc" />
         <Nav page={page} />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <p><a>Blu-Rave</a> is a full-stack application built by <a>Joe Gilberto</a> using <a>ASP.NET</a>, <a>React</a>, <a>PostgreSQL</a>, and <a>Auth0</a>.</p>
+        </footer>
+    );
+}
