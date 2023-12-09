644a9eea6447d30053713941be3e1d44e0ea4e4b
joekgilberto <joekgilberto@gmail.com>
2023-12-09 14:42:01 -0500

    adds links to footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 export default function Footer() {
     return (
         <footer>
-            <p><a>Blu-Rave</a> is a full-stack application built by <a>Joe Gilberto</a> using <a>ASP.NET</a>, <a>React</a>, <a>PostgreSQL</a>, and <a>Auth0</a>.</p>
+            <p><a href='https://github.com/joekgilberto/blu-rave' target='_blank'>Blu-Rave</a> is a full-stack application built by <a href='https://joekgilberto.com/' target='_blank'>Joe Gilberto</a> using <a href='https://dotnet.microsoft.com/en-us/apps/aspnet' target='_blank'>ASP.NET</a>, <a href='https://react.dev/' target='_blank'>React</a>, <a href='https://www.postgresql.org/' target='_blank'>PostgreSQL</a>, and <a href='https://auth0.com/' target='_blank'>Auth0</a>.</p>
         </footer>
     );
 }
