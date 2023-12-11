0bf36780a622987bc7cd543bd68454088e1e41e2
joekgilberto <joekgilberto@gmail.com>
2023-12-10 23:52:08 -0500

    stashing code

--- service/Models/BluRay.cs
+++ service/Models/BluRay.cs
@@ -6,37 +6,40 @@ namespace Models
     public class BluRay
     {
         [Column("id")]
-        public int Id { get; set; }
+        public required int Id { get; set; }
 
         [Column("title")]
-        public string Title { get; set; }
+        public required string Title { get; set; }
 
         [Column("year")]
         public int? Year { get; set; }
 
         [Column("steelbook")]
-        public bool Steelbook { get; set; }
+        public required bool Steelbook { get; set; }
 
         [Column("definition")]
-        public string Definition { get; set; }
+        public required string Definition { get; set; }
 
         [Column("format")]
-        public string Format { get; set; }
+        public required string Format { get; set; }
 
         [Column("notes")]
         public string? Notes { get; set; }
 
         [Column("date_added")]
-        public DateOnly DateAdded { get; set; }
+        public required DateOnly DateAdded { get; set; }
         
         [Column("owner")]
-        public string Owner { get; set; }
+        public required string Owner { get; set; }
         
         [Column("username")]
-        public string Username { get; set; }
+        public required string Username { get; set; }
 
         [Column("email")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
+
+        [Column("likes")]
+        public List<string>? Likes { get; set; }
 
         internal void Remove(string v)
         {
