36477d417ad913ead1f8cf856ecf057f9ca7af44
joekgilberto <joekgilberto@gmail.com>
2023-12-12 12:26:21 -0500

    adds range of dates to post feed

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -5,7 +5,7 @@ export default function Post({ bluRay, idx, listLength }) {
     return (
         <div className={`Post ${idx === listLength - 1 ? "last" : null}`}>
             <a href={`/feed/${bluRay.id}`}>
-                <p className='update'><span className='bold'>{bluRay.username}</span> collected <span className='bold italic'>{tools.putTheBack(bluRay.title)}</span>{bluRay.year ? ` (${bluRay.year})` : null} on {bluRay.definition}!</p>
+                <p className='update'><span className='bold'>{bluRay.username}</span> collected <span className='bold italic'>{tools.putTheBack(bluRay.title)}</span>{bluRay.year ? ` (${bluRay.year})` : bluRay.startYear && bluRay.endYear? ` (${bluRay.startYear}-${bluRay.endYear})` : bluRay.startYear && !bluRay.endYear ? ` (${bluRay.startYear})`:null} on {bluRay.definition}!</p>
             </a>
             <p className='added-on'>On {bluRay.dateAdded}</p>
         </div>
