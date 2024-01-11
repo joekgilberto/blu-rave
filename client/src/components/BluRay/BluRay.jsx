a445932719e17e4fe65de9592ab61c940ba48386
joekgilberto <joekgilberto@gmail.com>
2024-01-11 17:09:15 -0500

    adds icon to BluRay.jsx

--- client/src/components/BluRay/BluRay.jsx
+++ client/src/components/BluRay/BluRay.jsx
@@ -1,11 +1,13 @@
 import './BluRay.css';
+import * as tools from '../../utilities/tools';
 
-export default function BluRay({bluRay, idx, listLength, others}) {
+export default function BluRay({ bluRay, idx, listLength, others }) {
     return (
-        <a className="BluRay" href={others?`/feed/${bluRay.id}`:`/blu-rays/${bluRay.id}`}>
-            <div className={`media ${idx === listLength - 1 ? "last" : null}`}>
-                <p className='italic'>{bluRay.title}</p>
-                <p className='details'>{bluRay.definition == "4K" ? "4K" : bluRay.definition == "DVD" ? "DVD" : null} {bluRay.steelbook ? "★" : null}</p>
+        <a className={`BluRay ${idx === listLength - 1 ? "last" : null}`} href={others ? `/feed/${bluRay.id}` : `/blu-rays/${bluRay.id}`}>
+            <p className='italic'>{tools.putTheBack(bluRay.title)}</p>
+            <div className='details'>
+                <img className={`format ${bluRay.definition === "4K" ? "fourK" : null}`} src={bluRay.definition === "4K" ? "https://i.imgur.com/K9QlNj7.png" : bluRay.definition === "DVD" ? "https://i.imgur.com/n0Px4Ko.png" : "https://i.imgur.com/RKBncJe.png"} />
+                <p className='special'>{bluRay.steelbook ? "★" : null}</p>
             </div>
         </a>
     )
