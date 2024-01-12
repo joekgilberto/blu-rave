0fdc30908aa96cb09342e9928e545f5ab2325d56
joekgilberto <joekgilberto@gmail.com>
2024-01-11 20:49:29 -0500

    adds alt to definition icons

--- client/src/components/BluRay/BluRay.jsx
+++ client/src/components/BluRay/BluRay.jsx
@@ -6,7 +6,7 @@ export default function BluRay({ bluRay, idx, listLength, others }) {
         <a className={`BluRay ${idx === listLength - 1 ? "last" : null}`} href={others ? `/feed/${bluRay.id}` : `/blu-rays/${bluRay.id}`}>
             <p className='italic'>{tools.putTheBack(bluRay.title)}</p>
             <div className='details'>
-                <img className={`format ${bluRay.definition === "4K" ? "fourK" : null}`} src={bluRay.definition === "4K" ? "https://i.imgur.com/K9QlNj7.png" : bluRay.definition === "DVD" ? "https://i.imgur.com/n0Px4Ko.png" : "https://i.imgur.com/RKBncJe.png"} />
+                <img className={`format ${bluRay.definition === "4K" ? "fourK" : null}`} src={bluRay.definition === "4K" ? "https://i.imgur.com/K9QlNj7.png" : bluRay.definition === "DVD" ? "https://i.imgur.com/n0Px4Ko.png" : "https://i.imgur.com/RKBncJe.png"} alt={bluRay.definition === "4K" ? "4K" : bluRay.definition === "DVD" ? "DVD" : "Blu-Ray"} />
                 <p className='special'>{bluRay.steelbook ? "★" : null}</p>
             </div>
         </a>
